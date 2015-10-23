726e731784ff71218164f680322e2fe187bb370b
Josh Gillies <github@joshgilli.es>
2015-10-23 21:47:32 +1100

    Restructure project

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = function assets () {
   var asset = require('node-matrix-assets')
 
   return {
-    folder: require('./folder')(asset),
-    site: require('./site')(asset),
-    page: require('./page')(asset)
+    folder: require('./assets/folder')(asset),
+    site: require('./assets/site')(asset),
+    page: require('./assets/page')(asset)
   }
 }
