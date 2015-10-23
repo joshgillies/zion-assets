6bdb963080b2f25ac5e1dcd25d7f9b568a2381ad
Josh Gillies <github@joshgilli.es>
2015-10-23 21:48:40 +1100

    Enable BYO node-matrix-assets, and expose getAssetsById

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-module.exports = function assets () {
-  var asset = require('node-matrix-assets')
+module.exports = function assets (asset) {
+  asset = asset || require('node-matrix-assets')
 
   return {
     folder: require('./assets/folder')(asset),
     site: require('./assets/site')(asset),
-    page: require('./assets/page')(asset)
+    page: require('./assets/page')(asset),
+    getAssetById: asset.getAssetById
   }
 }
