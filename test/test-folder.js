37a249dd3a9bba279df73ed33c1a3d2edb68f342
Josh Gillies <github@joshgilli.es>
2015-11-05 23:49:21 +1100

    Add tests and fix children creation for folder assets

--- test/test-folder.js
+++ test/test-folder.js
@@ -1,12 +1,53 @@
 var test = require('tape')
 
 var za = require('../')
-var assets = za()
-var folder = assets.folder
 
 test('basic functionality', function (assert) {
+  var assets = za()
+  var folder = assets.folder
+
   var test = folder('test')
   assert.equal(test.name, 'test')
   assert.equal(test.type, 'folder')
+  assert.end()
+})
+
+test('create children', function (assert) {
+  var tests = {
+    'children as array': function () {
+      var assets = za()
+      var folder = assets.folder
+      var page = assets.page
+
+      var child1 = page('child1')
+      var child2 = page('child2')
+
+      return folder('parent', [child1, child2])
+    },
+    'children as second arg': function () {
+      var assets = za()
+      var folder = assets.folder
+      var page = assets.page
+
+      var child1 = page('child1')
+      var child2 = page('child2')
+
+      return folder('parent', child1, child2)
+    },
+    'children as final args': function () {
+      var assets = za()
+      var folder = assets.folder
+      var page = assets.page
+
+      var child1 = page('child1')
+      var child2 = page('child2')
+
+      return folder('parent', { link: 'type_2' }, child1, child2)
+    }
+  }
+
+  for (var test in tests) {
+    assert.equal(tests[test]().children.length, 2, test)
+  }
   assert.end()
 })
