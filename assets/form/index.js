8c6d98119f4b81fa91a0c6d858883e0657ec4c23
Josh Gillies <github@joshgilli.es>
2015-11-16 19:35:32 -0800

    Add base form definition

---
+++ assets/form/index.js
@@ -0,0 +1,20 @@
+module.exports = function context (asset) {
+  var base = require('../base')(asset)
+
+  return function form (name, opts) {
+    if (typeof name !== 'string') {
+      opts = name
+      name = undefined
+    }
+
+    if (!opts) {
+      opts = {}
+    }
+
+    if (!opts.name) {
+      opts.name = name
+    }
+
+    return base.apply(null, ['page_custom_form', opts].concat(Array.prototype.slice.call(arguments, 2)))
+  }
+}
