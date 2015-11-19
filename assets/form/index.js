ff6438f99faaa1bbe42be05f0da34bbedf690f44
Josh Gillies <github@joshgilli.es>
2015-11-18 22:55:58 -0800

    Add submissions folder to form asset definition

--- assets/form/index.js
+++ assets/form/index.js
@@ -14,7 +14,9 @@ module.exports = function context (asset) {
     }
 
     return asset('page_custom_form', opts, [
-      asset('form_email', { link: 'type_2', name: 'Form Contents', dependant: '1', exclusive: '1' })
+      asset('form_email', { link: 'type_2', name: 'Form Contents', dependant: '1', exclusive: '1' }, [
+        asset('folder', { link: { 'type_2': 'submissions_folder' }, name: 'Submissions', dependant: '1', exclusive: '1' })
+      ])
     ])
   }
 }
